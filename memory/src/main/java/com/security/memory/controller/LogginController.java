29f03cd7fc23596cbb7fd523c8463c82c508fcf8
Hector <comhector_@hotmail.com>
2025-06-18 22:54:11 -0600

    Integrando los login con usuarios y rutas para manejar distintos controllers con InMemory

--- memory/src/main/java/com/security/memory/controller/LogginController.java
+++ memory/src/main/java/com/security/memory/controller/LogginController.java
@@ -1,23 +1,41 @@
 package com.security.memory.controller;
 
-import org.springframework.security.access.method.P;
+
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@PreAuthorize("hasAutority('USER')")// Asegura que solo los usuarios con autoridad 'USER' puedan acceder a este controlador
-@RequestMapping("/api") // Define la ruta base para este controlador
+@RestController
+@RequestMapping("/auth") // Define la ruta base para este controlador
 public class LogginController {
 
-    @GetMapping("/loggin")
-    @PreAuthorize("hasAuthority('ADMIN')") // Asegura que solo los usuarios con autoridad 'ADMIN' puedan acceder a este endpoint
-    public String adminMenssage(){
-        return "Welcome to the admin page!"; // Mensaje de bienvenida para la página de administrador
+ 
+    @GetMapping("/private")
+    @PreAuthorize("hasAuthority('DEVELOPER')") // Asegura que solo los usuarios con autoridad 'DEVELOPER' puedan acceder a este endpoint
+    public String developerMenssage(){
+        return "Welcome to the DEVELOPER page!"; // Mensaje de bienvenida para la página de administrador
     }
+
+    @GetMapping("/public")
+    @PreAuthorize("hasAuthority('USER')") // Asegura que solo los usuarios
+    public String publicMenssage(){
+        return "Welcome to the PUBLIC page!"; // Mensaje de bienvenida para la página pública
+    }
+
+    @GetMapping("/api")
+    @PreAuthorize("hasAuthority('ADMIN')") // Asegura que solo los usuarios
+    public String adminMenssage(){
+        return "Welcome to the ADMIN page!"; // Mensaje de bienvenida para la página de administrador
+    }
+
+    @GetMapping("/actuator")
+    @PreAuthorize("hasAuthority('USER')") // Asegura que solo los usuarios
+    public String actuatorMenssage(){
+        return "Welcome to the ACTUATOR page!"; // Mensaje de bienvenida para la
+
     
 
-
+    }
 
 }
--- memory/src/main/java/com/security/memory/security/SecurityConfig.java
+++ memory/src/main/java/com/security/memory/security/SecurityConfig.java
@@ -3,14 +3,23 @@ package com.security.memory.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 
-@Configuration
-@EnableWebSecurity
+
+@Configuration // Marca esta clase como una clase de configuración de Spring, permitiendo la definición de beans y configuraciones
+@EnableWebSecurity // Habilita la configuración de seguridad web, permitiendo la personalización de la seguridad HTTP
+@EnableMethodSecurity // Habilita la seguridad a nivel de método, permitiendo el uso de anotaciones como @PreAuthorize
 public class SecurityConfig {
     
     @Bean
@@ -18,10 +27,10 @@ public class SecurityConfig {
         http
             .csrf((csrf) -> csrf.disable())//Desabilitado CSRF para permitir peticiones desde el cliente 
             .httpBasic((httpBasic) -> httpBasic.disable()) //Desabilitado HTTP Basic para evitar autenticación básica
-            .formLogin(null) //Desabilitado Form Login para evitar autenticación basada en formularios
+            .formLogin(withDefaults()) //Habilitado el formulario de inicio de sesión por defecto
             .authorizeHttpRequests((auth) -> auth // Configuracion de autenticación y autorizacion
-/*                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() //Permitir acceso a recursos estáticos comunes */
-                .requestMatchers("/api").hasAuthority("Admin")//Permitir acceso a la ruta /api solo para usuarios con autoridad Admin
+                .requestMatchers("login","/css/", "/js/", "/images/").permitAll() //Permitir acceso a la ruta de inicio de sesión sin autenticación
+                .requestMatchers("/api").hasAuthority("ADMIN")//Permitir acceso a la ruta /api solo para usuarios con autoridad Admin
                 .requestMatchers("/private").hasAuthority("DEVELOPER")//Permitir acceso a la ruta  solo a usuarios DEVELOPER
                 .requestMatchers("/public").authenticated()//Permitir aceso a la ruta a culaquier usuario autentificado
                 .requestMatchers("/actuator/**").hasAuthority("USER") //Permitir acceso a los endpoints de Actuator
@@ -30,5 +39,28 @@ public class SecurityConfig {
             
             return http.build();
             
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+            .username("user")
+            .password("2080")
+            .authorities("USER")
+            .build();
+
+        UserDetails admin = users
+            .username("admin")
+            .password("4080")
+            .authorities("ADMIN")
+            .build();
+
+        UserDetails developer = users
+            .username("developer")
+            .password("6080")
+            .authorities("DEVELOPER")
+            .build();
+        return new InMemoryUserDetailsManager(user, admin, developer);
     }
 }
